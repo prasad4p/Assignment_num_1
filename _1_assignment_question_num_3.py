0bdcf29d2119ff83a06c4f9bec55f523f2678984
prasad4p <122027371+prasad4p@users.noreply.github.com>
2023-01-15 13:07:05 +0530

    assignment_num1_question_3

---
+++ _1_assignment_question_num_3.py
@@ -0,0 +1,19 @@
+# Assignment_number_1
+# Question_number_3 = write the python program to count the number of even and odd number from a series of number.
+
+
+numbers = (3,8,11,19,27,30,35,48,55,74)
+even_number = 0
+odd_number = 0
+for i in numbers:
+        if not i % 2:
+             even_number += 1
+        else:
+             odd_number += 1
+print("Number of even numbers :" , even_number)
+print("Number of odd numbers :" , odd_number )
+
+
+   
+
+  
