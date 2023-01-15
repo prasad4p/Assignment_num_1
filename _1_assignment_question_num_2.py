3dd0da8ff839a82b1cdcdeced57c51d095894679
prasad4p <122027371+prasad4p@users.noreply.github.com>
2023-01-15 13:06:04 +0530

    assignment_num1_question_2

---
+++ _1_assignment_question_num_2.py
@@ -0,0 +1,6 @@
+# Assignment_number_1
+# Question_number_2 = write the python program that accept a word from the user and reverse it.
+
+word = input ("Enter the word : ")
+reverse_word = word [::-1]
+print ("Reverse of word  :", reverse_word)
